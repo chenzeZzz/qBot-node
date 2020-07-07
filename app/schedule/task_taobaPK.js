4dff2639392a703360b049d122472fad02b2e86f
Ze Chen <ze.chen@autodesk.com>
2020-07-07 19:18:33 +0800

    feat: open pk

--- app/schedule/task_taobaPK.js
+++ app/schedule/task_taobaPK.js
@@ -7,7 +7,7 @@ class TaobaPK extends Subscription {
   // 通过 schedule 属性来设置定时任务的执行间隔等配置
   static get schedule() {
     return {
-      disable: true,
+      disable: false,
       interval: '1m', // 1 分钟间隔
       immediate: true,
       type: 'worker', // 指定所有的 worker 都需要执行
