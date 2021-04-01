2c1b97e54611430759ef7b8f3170f795872cbbc4
Ze Chen <ze.chen@autodesk.com>
2021-04-01 17:38:21 +0800

    fix: get rank info is static.

--- app/schedule/task_taobaPK.js
+++ app/schedule/task_taobaPK.js
@@ -24,22 +24,11 @@ class TaobaPK extends Subscription {
     }
 
     const orderList = await this.service.http.getRankInfoFromTaoba(taobaId);
+    console.log('orderList====', orderList.length);
     if (!orderList || !orderList.length) return;
     // 取最新的一条，判断是否为新增.
-    let skip = true;
-    try {
-      skip =
-        (Number(orderList[0].stime) + 60 * 60 * 24) * 1000 <
-        new Date().getTime();
-    } catch (error) {
-      skip = false;
-      this.errLog('桃叭 list 判断失败======' + error);
-    }
-    if (skip) return; // 没新增
 
-    const lastRecordInDb = (await ctx.service.taoba.getLastTaobaRecordByTaobaId(
-      taobaId
-    )) || { stime: 0 };
+    const recordInDb = await ctx.service.taoba.getAll(taobaId);
     // { id: 53983,
     //   sn: '7ltlxf',
     //   money: '20.00',
@@ -51,53 +40,26 @@ class TaobaPK extends Subscription {
     //    'https://tvax3.sinaimg.cn/crop.0.14.751.751.1024/9cceebdfly8fi6u6z0dm3j20kv0lo0uz.jpg?KID=imgbed,tva&Expires=1587043178&ssig=CdI3pS0OMR'
     // }
     const records = [];
-    const lastStimeInDb = Number(new Date(lastRecordInDb.stime).getTime());
     orderList.forEach(item => {
-      if (lastStimeInDb < Number(item.stime) * 1000) {
-        records.push({
-          taobaId,
-          listId: item.id,
-          money: item.money,
-          uid: item.userid,
-          nick: item.nick,
-          stime: item.stime * 1000,
-        });
-      }
-    });
+      const pastRankItem = _.find(recordInDb, { uid: String(item.userid) });
 
+      records.push({
+        taobaId,
+        listId: item.id,
+        money: item.money,
+        addMony: pastRankItem ? Number(item.money) - Number(pastRankItem.money) : Number(item.money), // 这次刷新新增
+        uid: item.userid,
+        nick: item.nick,
+        stime: item.stime * 1000,
+      });
+    });
     if (!records.length) return;
 
+    // delete current
+    await ctx.service.taoba.deleteAllByTaobaoId(taobaId);
     // save db
     await ctx.service.taoba.savaTaoba(records);
 
-    //   [
-    //     {
-    //         "id": 4179,
-    //         "sid": 839,
-    //         "ads": "https://s0.tao-ba.club/upfile/idols/202006/aUFo60fX.png",
-    //         "title": "【王晓佳篇】穿过水晶的光芒",
-    //         "expire": 1591545540,
-    //         "donation": 14471,
-    //         "site": {
-    //             "nickname": "SNH48-王晓佳应援会",
-    //             "safedeposit": 0,
-    //             "avatar": "https://www.taoba.club/img/grab?s=RURSN2B9+Hgyq3iL0d8JrFRNX3ALVQTDEpRa/3UD4AEZP2EhdPXEPt3pOWagKIpnHbq5v4BUSchs68+csLMkdf9g0lwxHnuT&_=.jpg"
-    //         }
-    //     },
-    //     {
-    //         "id": 4178,
-    //         "sid": 634,
-    //         "ads": "https://s0.tao-ba.club/upfile/idols/202006/jkpQ1pMQ.png",
-    //         "title": "【冉蔚总选1.0】穿过水晶的光芒",
-    //         "expire": 1591545540,
-    //         "donation": 14172,
-    //         "site": {
-    //             "nickname": "SNH48-冉蔚应援会",
-    //             "safedeposit": 0,
-    //             "avatar": "https://www.taoba.club/img/grab?s=lKZiDMuKqFhmPMGSRxnv7ChnRU+WggfVqrLadcIUpTlXUjBraKBasCvmor8vva/5k1qt3tMiXwbo0cYnEMqzDUlebRQDbehd&_=.jpg"
-    //         }
-    //     }
-    // ]
     const pkstats = await this.app.getPkstatsFromTaoba();
     let rankIndex = -1;
     pkstats.forEach((item, index) => {
@@ -117,22 +79,24 @@ class TaobaPK extends Subscription {
     ).toFixed(2)}\n` : '';
 
     records.forEach(iterator => {
-      const msg =
-        `感谢 ${iterator.nick} 刚刚在${masterItem.title}中支持了：${iterator.money}元！ \n` +
-        `已筹: ${masterItem.donation} 元\n` +
-        `排名: ${rankIndex + 1} 名\n` +
-        `${rankInfo}` +
-        `集资截止时间: ${moment(masterItem.expire * 1000).format(
-          'YYYY-MM-DD'
-        )} \n` +
-        `集资链接: ${this.app.config.target_site_origin + this.app.config.taoba.taobaPKId} \n` +
-        '输入 `集资` 或者 `jz` 查看详情';
-      this.app.socket_qbot.send(
-        this.app.config.genMsg('send_group_msg', {
-          group_id: this.app.config.group_id,
-          message: msg,
-        })
-      );
+      if (iterator.addMony > 0) {
+        const msg =
+          `感谢 ${iterator.nick} 刚刚在${masterItem.title}中支持了：${iterator.addMony.toFixed(2)}元！ \n` +
+          `已筹: ${masterItem.donation} 元\n` +
+          `排名: ${rankIndex + 1} 名\n` +
+          `${rankInfo}` +
+          `集资截止时间: ${moment(masterItem.expire * 1000).format(
+            'YYYY-MM-DD'
+          )} \n` +
+          `集资链接: ${this.app.config.target_site_origin + this.app.config.taoba.taobaPKId} \n` +
+          '输入 `集资` 或者 `jz` 查看详情';
+        this.app.socket_qbot.send(
+          this.app.config.genMsg('send_group_msg', {
+            group_id: this.app.config.group_id,
+            message: msg,
+          })
+        );
+      }
     });
 
   }
